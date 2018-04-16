3300ff491169700cd7004460d7c318b2897f4fa5
Renzo Lucioni <renzo@lucioni.xyz>
2018-04-15 23:34:17 -0400

    configure logging for clean script

--- clean.py
+++ clean.py
@@ -1,6 +1,32 @@
 import logging
+from logging.config import dictConfig
 
 import boto3
+
+dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '{asctime} {levelname} {process} [{filename}:{lineno}] - {message}',
+            'style': '{',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+})
 
 logger = logging.getLogger(__name__)
 
